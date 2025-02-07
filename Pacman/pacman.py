0fd8e1586e299e6decc7ba5d0870c4e2da458072
Nikita Borysov <nborysov15@gmail.com>
2025-02-07 20:39:48 +0200

    Pacman movement on the board

--- Pacman/pacman.py
+++ Pacman/pacman.py
@@ -25,6 +25,9 @@ direction = 0
 counter = 0
 flicker = False
 turns_allowed = [False, False, False, False]
+direction_command = 0
+pacman_speed = 2
+
 
 def draw_board():
     num1 = ((height - 50) // 32)
@@ -101,12 +104,12 @@ def check_position(centerx, centery):
                     turns[0] = True
 
         if direction == 0 or direction == 1:
-            if 12 <= centerx % num2 <= 18:
+            if 9 <= centerx % num2 <= 15:
                 if level[(centery + num1) // num1][centerx // num2] < 3:
                     turns[3] = True
                 if level[(centery - num1) // num1][centerx // num2] < 3:
                     turns[2] = True
-            if 12 <= centery % num1 <= 18:
+            if 9 <= centery % num1 <= 15:
                 if level[centery // num1][(centerx - num3) // num2] < 3:
                     turns[1] = True
                 if level[centery // num1][(centerx + num3) // num2] < 3:
@@ -117,6 +120,18 @@ def check_position(centerx, centery):
         turns[0] = True
         turns[1] = True
     return turns
+
+def move_pacman(pac_x, pac_y):
+    # 0-r, 1-l, 2-u, 3-down
+    if direction == 0 and turns_allowed[0]:
+        pac_x += pacman_speed
+    elif direction == 1 and turns_allowed[1]:
+        pac_x -= pacman_speed
+    elif direction == 2 and turns_allowed[2]:
+        pac_y -= pacman_speed
+    elif direction == 3 and turns_allowed[3]:
+        pac_y += pacman_speed
+    return pac_x, pac_y
 
 
 run = True
@@ -136,19 +151,40 @@ while run:
     center_x = pacman_x + 16
     center_y = pacman_y + 17
     turns_allowed = check_position(center_x, center_y)
+    pacman_x, pacman_y = move_pacman(pacman_x, pacman_y)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                direction = 0
+                direction_command = 0
             if event.key == pygame.K_LEFT:
-                direction = 1
+                direction_command = 1
             if event.key == pygame.K_UP:
-                direction = 2
+                direction_command = 2
             if event.key == pygame.K_DOWN:
-                direction = 3
+                direction_command = 3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT and direction_command == 0:
+                direction_command = direction
+            if event.key == pygame.K_LEFT and direction_command == 1:
+                direction_command = direction
+            if event.key == pygame.K_UP and direction_command == 2:
+                direction_command = direction
+            if event.key == pygame.K_DOWN and direction_command == 3:
+                direction_command = direction
+
+    for i in range(4):
+        if direction_command == i and turns_allowed[i]:
+            direction = i
+
+    if pacman_x > 720:
+        pacman_x = -35
+    elif pacman_x < -38:
+        pacman_x = 717
+
 
     pygame.display.flip()
 
