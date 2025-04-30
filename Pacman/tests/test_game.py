b20e9ab0ec96a1ac1571f7364b3260308be41ab2
perpetuumddt <perpetuumddt@gmail.com>
2025-04-30 18:02:16 +0300

    added comments to tests

--- Pacman/tests/test_game.py
+++ Pacman/tests/test_game.py
@@ -2,16 +2,20 @@ import pytest
 import pygame
 from game import Game
 
+#fixture creates a new instance of the game class before each test
 @pytest.fixture
 def game_instance():
     screen = pygame.Surface((720, 780))
     return Game(screen, exit_callback=lambda: None, current_level=1)
 
+#Test: Game initialization should set default values.
 def test_game_initialization(game_instance):
     assert game_instance.lives == 3
     assert game_instance.score == 0
     assert not game_instance.powerup
 
+#Test: Reset game method should reset game attributes
+# simulates attribute change, then restarts game state and checks values
 def test_reset_game_state(game_instance):
     game_instance.lives = 1
     game_instance.score = 100
