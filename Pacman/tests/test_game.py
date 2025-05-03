591be18a5fb7644da9b161ab8fb60bd17c88f2ea
Nikita Borysov <nborysov15@gmail.com>
2025-05-03 15:38:53 +0300

    fixed tests

--- Pacman/tests/test_game.py
+++ Pacman/tests/test_game.py
@@ -73,14 +73,11 @@ def test_pacman_move(pacman_instance, direction, turns_allowed, expected_x, expe
 
 # Test: Pacman screen wrapping
 def test_pacman_screen_wrap(pacman_instance):
-    pacman_instance.x = 691
-    pacman_instance.direction = 0  # Move right
-    pacman_instance.turns_allowed = [True, True, True, True]
+    pacman_instance.x = 692
     pacman_instance.move()
     assert pacman_instance.x == -35
 
-    pacman_instance.x = -38
-    pacman_instance.direction = 1  # Move left
+    pacman_instance.x = -39
     pacman_instance.move()
     assert pacman_instance.x == 691
 
